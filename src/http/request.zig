4131e2a3f93fa0912fa477a09d3503dff7e0eadb
AndrewPochapsky <andrew.pochapsky@gmail.com>
2025-03-08 11:22:20 -0800

    Refactor to allocate all memory upfront on the stack and put logic into parser

--- src/http/request.zig
+++ src/http/request.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+pub const Error = error{UnknownMethod};
+
+pub const MAX_MESSAGE_SIZE = 1024 * 8 + 1024 * 1000;
+
 pub const Method = enum {
     Get,
     Head,
@@ -10,3 +14,52 @@ pub const Request = struct {
     method: Method,
     path: []const u8,
 };
+
+const RequestParser = struct {
+    index: usize,
+    message: [MAX_MESSAGE_SIZE]u8,
+    message_len: usize,
+
+    fn parse(self: *@This()) Error!Request {
+        const method = try self.parseMethod();
+        return Request{ .method = method, .path = "" };
+    }
+
+    fn parseMethod(self: *@This()) Error!Method {
+        const firstBytes = self.readBytes(3);
+        if (bytesEql(firstBytes, "GET")) {
+            return Method.Get;
+        } else if (bytesEql(firstBytes, "POS")) {
+            const nextByte = self.readByte();
+            if (nextByte == 'T') {
+                return Method.Post;
+            }
+        } else if (bytesEql(firstBytes, "HEA")) {
+            const nextByte = self.readByte();
+            if (nextByte == 'D') {
+                return Method.Head;
+            }
+        }
+        return Error.UnknownMethod;
+    }
+
+    fn readByte(self: *@This()) u8 {
+        self.index += 1;
+        return self.message[self.index - 1];
+    }
+
+    fn readBytes(self: *@This(), num: usize) []const u8 {
+        const slice = self.message[self.index .. self.index + num];
+        self.index += num;
+        return slice;
+    }
+};
+
+fn bytesEql(bytes: []const u8, other: []const u8) bool {
+    return std.mem.eql(u8, bytes, other);
+}
+
+pub fn parseMessage(message: [MAX_MESSAGE_SIZE]u8, message_len: usize) Error!Request {
+    var parser: RequestParser = .{ .index = 0, .message = message, .message_len = message_len };
+    return try parser.parse();
+}
--- src/main.zig
+++ src/main.zig
@@ -8,12 +8,10 @@ const Method = http_request.Method;
 
 const PORT = 1234;
 
-const Error = error{UnknownMethod};
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    //const allocator = gpa.allocator();
 
     const loopback = try net.Ip4Address.parse("127.0.0.1", PORT);
     const localhost = net.Address{ .in = loopback };
@@ -30,33 +28,9 @@ pub fn main() !void {
 
     print("Connection received! {} is sending data.\n", .{client.address});
 
-    try parseRequest(client);
-    const message: []const u8 = try client.stream.reader().readAllAlloc(allocator, 1024);
-    defer allocator.free(message);
+    var buffer = std.mem.zeroes([http_request.MAX_MESSAGE_SIZE]u8);
+    const message_len: usize = try client.stream.reader().readAll(&buffer);
+    const request: HttpRequest = try http_request.parseMessage(buffer, message_len);
 
-    print("{} says {s}\n", .{ client.address, message });
-}
-
-fn parseRequest(connection: net.Server.Connection) !void {
-    const method = try parseMethod(connection);
-    print("method: {}\n", .{method});
-}
-
-fn parseMethod(connection: net.Server.Connection) !Method {
-    const reader = connection.stream.reader();
-    const firstBytes = try reader.readBytesNoEof(3);
-    if (std.mem.eql(u8, &firstBytes, "GET")) {
-        return Method.Get;
-    } else if (std.mem.eql(u8, &firstBytes, "POS")) {
-        const nextByte = try reader.readByte();
-        if (nextByte == 'T') {
-            return Method.Post;
-        }
-    } else if (std.mem.eql(u8, &firstBytes, "HEA")) {
-        const nextByte = try reader.readByte();
-        if (nextByte == 'D') {
-            return Method.Head;
-        }
-    }
-    return Error.UnknownMethod;
+    print("Request: {}", .{request});
 }
