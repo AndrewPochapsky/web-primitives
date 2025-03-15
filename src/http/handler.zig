674aa048c96d99c091c7f363476dc788936fe579
AndrewPochapsky <andrew.pochapsky@gmail.com>
2025-03-14 21:07:40 -0700

    Upgrade to zig 0.14.0

--- src/http/handler.zig
+++ src/http/handler.zig
@@ -5,16 +5,18 @@ const Method = request.Method;
 
 const RESPONSE_TEMPLATE = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: {d}\r\n\r\n{s}";
 
+const ArrayList = std.ArrayListUnmanaged;
+
 pub fn handleRequest(allocator: std.mem.Allocator, req: Request) ![]u8 {
     switch (req.method) {
         Method.Get => {
             const file_path = try std.fmt.allocPrint(allocator, "src/app{s}", .{req.path});
             defer allocator.free(file_path);
-            const file_contents = readFile(file_path) catch |err| {
+            var file_contents = readFile(allocator, file_path) catch |err| {
                 std.debug.print("Error getting file: {s}, {}\n", .{ file_path, err });
                 return "";
             };
-            defer file_contents.deinit();
+            defer file_contents.deinit(allocator);
             const content_length = file_contents.items.len;
             const return_message = try std.fmt.allocPrint(allocator, RESPONSE_TEMPLATE, .{ content_length, file_contents.items });
             return return_message;
@@ -25,21 +27,21 @@ pub fn handleRequest(allocator: std.mem.Allocator, req: Request) ![]u8 {
     return undefined;
 }
 
-fn readFile(file_path: []const u8) !std.ArrayList(u8) {
+fn readFile(allocator: std.mem.Allocator, file_path: []const u8) !ArrayList(u8) {
     var file = try std.fs.cwd().openFile(file_path, .{});
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
     var buf: [1024]u8 = undefined;
 
-    var output: std.ArrayList(u8) = std.ArrayList(u8).init(std.heap.page_allocator);
+    var output: ArrayList(u8) = .empty;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         var i: u32 = 0;
         while (i < line.len) {
-            try output.append(line[i]);
+            try output.append(allocator, line[i]);
             i += 1;
         }
-        try output.append('\n');
+        try output.append(allocator, '\n');
     }
 
     return output;
