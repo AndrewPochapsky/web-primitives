7f48fdbb8942aade92177ee5f77bf6f453d07a8e
AndrewPochapsky <andrew.pochapsky@gmail.com>
2025-03-14 20:30:36 -0700

    Refactor logic into server.zig

---
+++ src/http/server.zig
@@ -0,0 +1,52 @@
+const std = @import("std");
+const net = std.net;
+const print = std.debug.print;
+
+const http_request = @import("request.zig");
+const HttpRequest = http_request.Request;
+const Method = http_request.Method;
+const handler = @import("handler.zig");
+
+pub const Server = struct {
+    server: net.Server,
+
+    pub fn start(self: *@This(), allocator: std.mem.Allocator) !void {
+        print("Listening on {}, access this port to end the program\n", .{self.server.listen_address.getPort()});
+        while (true) {
+            try self.handleRequest(allocator);
+        }
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.server.deinit();
+    }
+
+    fn handleRequest(self: *@This(), allocator: std.mem.Allocator) !void {
+        var client: net.Server.Connection = try self.server.accept();
+        defer client.stream.close();
+
+        print("Connection received! {} is sending data.\n", .{client.address});
+        var buffer = std.mem.zeroes([http_request.MAX_MESSAGE_SIZE]u8);
+        const message_len: usize = try client.stream.reader().read(&buffer);
+        var request_result = http_request.parseMessage(allocator, buffer, message_len);
+        if (request_result) |*request| {
+            defer request.deinit();
+
+            const return_message = try handler.handleRequest(allocator, request.*);
+            defer allocator.free(return_message);
+
+            _ = try client.stream.writer().writeAll(return_message);
+        } else |err| {
+            print("Error parsing request {}\n", .{err});
+        }
+    }
+};
+
+pub fn init(port: u16) !Server {
+    const loopback = try net.Ip4Address.parse("127.0.0.1", port);
+    const localhost = net.Address{ .in = loopback };
+    const server = try localhost.listen(.{
+        .reuse_address = true,
+    });
+    return .{ .server = server };
+}
--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,7 @@ const http_request = @import("http/request.zig");
 const HttpRequest = http_request.Request;
 const Method = http_request.Method;
 const handler = @import("http/handler.zig");
+const s = @import("http/server.zig");
 
 const PORT = 1234;
 
@@ -14,32 +15,8 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const loopback = try net.Ip4Address.parse("127.0.0.1", PORT);
-    const localhost = net.Address{ .in = loopback };
-    var server = try localhost.listen(.{
-        .reuse_address = true,
-    });
+    var server = try s.init(PORT);
     defer server.deinit();
 
-    const addr = server.listen_address;
-    print("Listening on {}, access this port to end the program\n", .{addr.getPort()});
-    while (true) {
-        var client: net.Server.Connection = try server.accept();
-        defer client.stream.close();
-
-        print("Connection received! {} is sending data.\n", .{client.address});
-        var buffer = std.mem.zeroes([http_request.MAX_MESSAGE_SIZE]u8);
-        const message_len: usize = try client.stream.reader().read(&buffer);
-        var request_result = http_request.parseMessage(allocator, buffer, message_len);
-        if (request_result) |*request| {
-            defer request.deinit();
-
-            const return_message = try handler.handleRequest(allocator, request.*);
-            defer allocator.free(return_message);
-
-            _ = try client.stream.writer().writeAll(return_message);
-        } else |err| {
-            print("Error parsing request {}\n", .{err});
-        }
-    }
+    try server.start(allocator);
 }
