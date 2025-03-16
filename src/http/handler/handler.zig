125eb6f66e9e268d4978d726065012c6f005e7c9
AndrewPochapsky <andrew.pochapsky@gmail.com>
2025-03-15 21:04:53 -0700

    Clean up

--- src/http/handler/handler.zig
+++ src/http/handler/handler.zig
@@ -1,11 +1,6 @@
 const std = @import("std");
 const request = @import("../request.zig");
 const Request = request.Request;
-const Method = request.Method;
-
-const RESPONSE_TEMPLATE = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: {d}\r\n\r\n{s}";
-const NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 48\r\nConnection: close\r\n\r\n<!DOCTYPE html> <html><body>404 Not Found</body></html>";
-const ArrayList = std.ArrayListUnmanaged;
 
 pub const Response = union(enum) {
     allocated: []u8,
