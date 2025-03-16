27f1b9dd8548f45a71854af90f6ac867ba3a3781
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2025-03-16 17:38:47 +0000

    Improve response type (#2)

--- src/http/handler/html_handler.zig
+++ src/http/handler/html_handler.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 const handler = @import("handler.zig");
 const Handler = handler.Handler;
-const Response = handler.Response;
+const response = @import("../response.zig");
+const Response = response.Response;
 const request = @import("../request.zig");
 const Request = request.Request;
 const Method = request.Method;
 
-const RESPONSE_TEMPLATE = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\nContent-Length: {d}\r\n\r\n{s}";
-const NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 48\r\nConnection: close\r\n\r\n<!DOCTYPE html> <html><body>404 Not Found</body></html>";
-const ArrayList = std.ArrayListUnmanaged;
+const NOT_FOUND_BODY = "<!DOCTYPE html> <html><body>404 Not Found</body></html>";
 
 pub const HtmlHandler = struct {
     pub fn handle(_: *HtmlHandler, allocator: std.mem.Allocator, req: Request) anyerror!Response {
@@ -16,14 +15,19 @@ pub const HtmlHandler = struct {
             Method.Get => {
                 const file_path = try std.fmt.allocPrint(allocator, "src/app{s}", .{req.path});
                 defer allocator.free(file_path);
-                var file_contents = readFile(allocator, file_path) catch |err| {
+                const file_contents = readFile(allocator, file_path) catch |err| {
                     std.debug.print("Error getting file: {s}, {}\n", .{ file_path, err });
-                    return .{ .static = NOT_FOUND_RESPONSE };
+                    return .{
+                        .status_code = .not_found,
+                        .content_type = .html,
+                        .body = .{ .static = NOT_FOUND_BODY },
+                    };
                 };
-                defer file_contents.deinit(allocator);
-                const content_length = file_contents.items.len;
-                const return_message = try std.fmt.allocPrint(allocator, RESPONSE_TEMPLATE, .{ content_length, file_contents.items });
-                return .{ .allocated = return_message };
+                return .{
+                    .status_code = .ok,
+                    .content_type = .html,
+                    .body = .{ .allocated = file_contents },
+                };
             },
             Method.Post => {},
             Method.Head => {},
@@ -36,22 +40,7 @@ pub const HtmlHandler = struct {
     }
 };
 
-fn readFile(allocator: std.mem.Allocator, file_path: []const u8) !ArrayList(u8) {
+fn readFile(allocator: std.mem.Allocator, file_path: []const u8) ![]u8 {
     var file = try std.fs.cwd().openFile(file_path, .{});
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
-    var buf: [1024]u8 = undefined;
-
-    var output: ArrayList(u8) = .empty;
-
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var i: u32 = 0;
-        while (i < line.len) {
-            try output.append(allocator, line[i]);
-            i += 1;
-        }
-        try output.append(allocator, '\n');
-    }
-
-    return output;
+    return try file.reader().readAllAlloc(allocator, 1024);
 }
